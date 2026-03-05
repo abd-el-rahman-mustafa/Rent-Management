53014888404cb35556b698e253fabf4aa682241d
Abd El-Rahman Mustafa <aamus2024@gmail.com>
2026-03-05 18:21:55 +0400

    feat: add routing for authentication and main page, and create environment configuration

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/auth/login/login';
+import { Register } from './pages/auth/register/register';
+import { MainPage } from './pages/main-page/main-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'register', component: Register },
+  { path: 'login', component: Login },
+  { path: '', component: MainPage },
+
+];
