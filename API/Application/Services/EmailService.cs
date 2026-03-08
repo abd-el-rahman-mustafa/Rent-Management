32c61ab966f38fcf0736317f2b333da9aaee0d23
Abd El-Rahman Mustafa <aamus2024@gmail.com>
2026-03-08 20:26:39 +0400

    feat: implement EmailService for sending emails with MailKit

---
+++ API/Application/Services/EmailService.cs
@@ -0,0 +1,47 @@
+
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+
+namespace API.Application.Services;
+public interface IEmailService
+{
+    Task SendAsync(string toEmail, string subject, string body);
+}
+
+
+
+public class EmailService : IEmailService
+{
+    private readonly EmailSettings _settings;
+
+    public EmailService(IOptions<EmailSettings> options)
+    {
+        _settings = options.Value;
+    }
+
+    public async Task SendAsync(string toEmail, string subject, string body)
+    {
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress(_settings.SenderName, _settings.SenderEmail));
+        message.To.Add(MailboxAddress.Parse(toEmail));
+        message.Subject = subject;
+        message.Body = new TextPart("html") { Text = body };
+
+        using var client = new SmtpClient();
+        await client.ConnectAsync(_settings.Host, _settings.Port, SecureSocketOptions.StartTls);
+        await client.AuthenticateAsync(_settings.SenderEmail, _settings.Password);
+        await client.SendAsync(message);
+        await client.DisconnectAsync(true);
+    }
+}
+
+public class EmailSettings
+{
+    public string Host { get; set; }
+    public int Port { get; set; }
+    public string SenderEmail { get; set; }
+    public string SenderName { get; set; }
+    public string Password { get; set; }
+}
