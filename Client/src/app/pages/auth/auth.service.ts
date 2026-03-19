a365b42f58b8032ae2689a7d105d1e70e3087ead
Abd El-Rahman Mustafa <aamus2024@gmail.com>
2026-03-19 20:22:05 +0400

    update

--- Client/src/app/pages/auth/auth.service.ts
+++ Client/src/app/pages/auth/auth.service.ts
@@ -11,23 +11,24 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
 
+  url = environment.API_URL + 'auth';
 
   constructor(private http: HttpClient) {
   }
 
   sendOtp(email: string) {
-    return this.http.post(`${environment.API_URL}auth/send-email-otp`, { email });
+    return this.http.post(`${this.url}/send-email-otp`, { email });
   }
 
   register(registerDtor: RegisterDto) {
-    return this.http.post(`${environment.API_URL}auth/register`, registerDtor);
+    return this.http.post(`${this.url}/register`, registerDtor);
   }
 
   loginRequest(loginDto: LoginDto): Observable<ApiResponse<string>> {
-    return this.http.post<ApiResponse<string>>(`${environment.API_URL}auth/login-request`, loginDto);
+    return this.http.post<ApiResponse<string>>(`${this.url}/login-request`, loginDto);
   }
-  emailOtpLogin(otpDto: LoginOtpDto) : Observable<ApiResponse<LoginResponse>> {
-    return this.http.post<ApiResponse<LoginResponse>>(`${environment.API_URL}auth/email-otp-login`, otpDto);
+  emailOtpLogin(otpDto: LoginOtpDto): Observable<ApiResponse<LoginResponse>> {
+    return this.http.post<ApiResponse<LoginResponse>>(`${this.url}/email-otp-login`, otpDto);
   }
 
 
