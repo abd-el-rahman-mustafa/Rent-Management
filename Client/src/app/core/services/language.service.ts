3d25a473b1621251ae36d0301def0a6498ad0274
Abd El-Rahman Mustafa <aamus2024@gmail.com>
2026-03-29 23:15:56 +0400

    add language service

---
+++ Client/src/app/core/services/language.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, signal } from '@angular/core';
+import { Lang } from '../interfaces/language.interface';
+
+
+@Injectable({ providedIn: 'root' })
+export class LanguageService {
+  lang = signal<Lang>(
+    (localStorage.getItem('lang') as Lang) ?? 'en'
+  );
+
+  setLang(lang: Lang) {
+    this.lang.set(lang);
+    localStorage.setItem('lang', lang);
+    document.dir = lang === 'ar' ? 'rtl' : 'ltr';
+  }
+}
