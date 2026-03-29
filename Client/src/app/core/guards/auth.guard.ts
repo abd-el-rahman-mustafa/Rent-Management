8fe1b6934106657cc140691d41c71ca77a9d87d0
Abd El-Rahman Mustafa <aamus2024@gmail.com>
2026-03-30 00:09:41 +0400

    update

--- Client/src/app/core/guards/auth.guard.ts
+++ Client/src/app/core/guards/auth.guard.ts
@@ -2,13 +2,13 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthToken } from '../interfaces/api.interface';
 import { TokenService } from '../services/token.service';
+import { LanguageService } from '../services/language.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  // Implement your authentication logic here
-  // inject Router to navigate to login page if not authenticated
-
   const router = inject(Router);
   const tokenService = inject(TokenService);
+  const lang = inject(LanguageService).lang();
+
   const token = tokenService.getToken();
   if (token) {
     const tokenData: AuthToken = token;
@@ -17,18 +17,14 @@ export const authGuard: CanActivateFn = (route, state) => {
       if (expirationDate > new Date()) {
         return true; // Token is valid
       } else {
-        tokenService.removeToken(); // Remove expired token
-        // Optionally, you can redirect the user to the login page here
-        router.navigate(['/login']);
-        return false; // Token has expired
+        tokenService.removeToken();
+        return router.createUrlTree([lang, 'login']); // Token has expired
       }
     } else {
-      router.navigate(['/login']);
-      return false; // No expiration time found
+      return router.createUrlTree([lang, 'login']); // No expiration time found
     }
   } else {
-    router.navigate(['/login']);
-    return false; // No token found
+    return router.createUrlTree([lang, 'login']); // No token found
   }
 };
 
@@ -36,20 +32,20 @@ export const authGuard: CanActivateFn = (route, state) => {
 export const notAuthGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const tokenService = inject(TokenService);
+  const lang = inject(LanguageService).lang();
+
   const token = tokenService.getToken();
   if (token) {
     const tokenData: AuthToken = token;
     if (tokenData.accessTokenExpires) {
       const accessTokenExpires = new Date(tokenData.accessTokenExpires);
       if (accessTokenExpires > new Date()) {
-        router.navigate(['/']); // Redirect to home page if authenticated
-        return false; // User is authenticated, prevent access to this route
+        return router.createUrlTree([lang]); // Redirect to home if authenticated
       } else {
         tokenService.removeToken();
-        router.navigate(['/login']);
-        return false; // Token has expired, prevent access to this route
+        return router.createUrlTree([lang, 'login']); // Token expired
       }
     }
   }
-  return true; // User is not authenticated, allow access to this route
+  return true; // User is not authenticated, allow access
 };
