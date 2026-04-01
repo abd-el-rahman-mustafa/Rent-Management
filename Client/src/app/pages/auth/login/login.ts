958ee34f5bce1f00b10b48e80ef8f0014a14c10d
Abd El-Rahman Mustafa <aamus2024@gmail.com>
2026-04-01 23:27:50 +0400

    login new design v1

--- Client/src/app/pages/auth/login/login.ts
+++ Client/src/app/pages/auth/login/login.ts
@@ -1,39 +1,38 @@
 import { TokenService } from './../../../core/services/token.service';
 import { AuthService } from './../auth.service';
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormInput } from '../../../shared/components/input/input';
 import { ToastrService } from 'ngx-toastr';
 import { otpValidator, passwordValidator } from '../../../shared/validators/validators';
-import { LoginDto, LoginOtpDto, RegisterDto } from '../auth.interface';
+import { LoginDto, LoginOtpDto } from '../auth.interface';
 import { Countdown } from '../../../shared/pipelines/countdown-pipe';
-import { Router } from '@angular/router';
-import { AuthToken } from '../../../core/interfaces/api.interface';
+import { Router, RouterLink } from '@angular/router';
 import { BaseComponent } from '../../../core/components/base-component/base-component';
 import { LangRouterLinkDirective } from '../../../core/directives/lang-router-link.directive';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, ReactiveFormsModule, FormInput, Countdown, LangRouterLinkDirective],
+  imports: [CommonModule, ReactiveFormsModule, FormInput, Countdown, RouterLink, LangRouterLinkDirective],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-//inherit from base component to get the language support
 export class Login extends BaseComponent {
   requestForm: FormGroup = new FormGroup({});
   otpLoginForm: FormGroup = new FormGroup({});
 
   otpSent = false;
-  otpCountdown = 60; // 60 seconds
+  otpCountdown = 60;
   intervalId: any;
+
+  // Used in the brand panel footer
+  readonly currentYear = signal(new Date().getFullYear());
 
   toastr = inject(ToastrService);
   router = inject(Router);
   tokenService = inject(TokenService);
-  /**
-   *
-   */
+
   constructor(
     private authService: AuthService,
     private fb: FormBuilder,
@@ -44,6 +43,7 @@ export class Login extends BaseComponent {
   ngOnInit(): void {
     this.initializeForm();
   }
+
   initializeForm() {
     this.requestForm = this.fb.nonNullable.group({
       email: ['', [Validators.required, Validators.email]],
@@ -55,28 +55,20 @@ export class Login extends BaseComponent {
   }
 
   startCountdown() {
-    this.otpCountdown = 60; // reset to 60 seconds
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
+    this.otpCountdown = 60;
+    if (this.intervalId) clearInterval(this.intervalId);
     this.intervalId = setInterval(() => {
       this.otpCountdown--;
-      if (this.otpCountdown <= 0) {
-        clearInterval(this.intervalId);
-      }
+      if (this.otpCountdown <= 0) clearInterval(this.intervalId);
     }, 1000);
   }
 
   sendLoginRequest() {
     if (this.requestForm.valid) {
-      const loginDto: LoginDto = {
-        ...this.requestForm.value,
-      };
-
+      const loginDto: LoginDto = { ...this.requestForm.value };
       this.authService.loginRequest(loginDto).subscribe({
         next: (response) => {
           this.toastr.success(response.detail, response.title);
-          console.log(response.detail);
           this.otpSent = true;
           this.startCountdown();
         },
@@ -92,13 +84,10 @@ export class Login extends BaseComponent {
         email: this.requestForm.value.email,
         otp: this.otpLoginForm.value.otp,
       };
-
       this.authService.emailOtpLogin(otpDto).subscribe({
         next: (response) => {
           this.toastr.success(response.detail, response.title);
           this.tokenService.setToken(response.data);
-
-          // navigate to home page or dashboard
           this.router.navigate(['/']);
         },
       });
@@ -108,24 +97,16 @@ export class Login extends BaseComponent {
   }
 
   reSendOtp() {
-    const loginDto: LoginDto = {
-      ...this.requestForm.value,
-    };
+    const loginDto: LoginDto = { ...this.requestForm.value };
     this.authService.loginRequest(loginDto).subscribe({
       next: (response) => {
         this.toastr.success(response.detail, response.title);
-        console.log(response.detail);
         this.startCountdown();
-      },
-      error: (error) => {
-        // this.toastr.error(error.error.message, 'Error');
       },
     });
   }
 
   ngOnDestroy() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
+    if (this.intervalId) clearInterval(this.intervalId);
   }
 }
