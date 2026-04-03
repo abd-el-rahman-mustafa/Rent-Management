f7d81cf761157ab2858cdba0d408b393c1f094a1
Abd El-Rahman Mustafa <aamus2024@gmail.com>
2026-04-03 22:58:04 +0400

    update

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/auth/login/login';
 import { Register } from './pages/auth/register/register';
-import { MainPage } from './pages/main-page/main-page';
 import { authGuard, notAuthGuard } from './core/guards/auth.guard';
-import { UsersLit } from './pages/user/users-lit/users-lit';
 import { langGuard } from './core/guards/lang.guard';
 import { redirectToLangGuard } from './core/guards/redirect-to-lang.guard';
 import { Layout } from './pages/layout/layout';
@@ -16,12 +14,12 @@ export const routes: Routes = [
     children: [
       { path: 'register', component: Register, canActivate: [notAuthGuard] },
       { path: 'login', component: Login, canActivate: [notAuthGuard] },
-      // layouted routes
       {
         path: '', component: Layout, canActivate: [authGuard],
         children: [
-          // main page
-          { path: 'dashboard', loadComponent: () => import('./pages/main-page/main-page').then(m => m.MainPage) },
+          { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+          // dashboard
+          { path: 'dashboard', loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard) },
 
           // users
           { path: 'users', loadComponent: () => import('./pages/user/users-lit/users-lit').then(m => m.UsersLit) }
--- Client/src/app/pages/dashboard/dashboard.spec.ts
+++ Client/src/app/pages/dashboard/dashboard.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MainPage } from './main-page';
+import { Dashboard } from './dashboard';
 
-describe('MainPage', () => {
-  let component: MainPage;
-  let fixture: ComponentFixture<MainPage>;
+describe('Dashboard', () => {
+  let component: Dashboard;
+  let fixture: ComponentFixture<Dashboard>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MainPage]
+      imports: [Dashboard]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(MainPage);
+    fixture = TestBed.createComponent(Dashboard);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
